9a8413d644eed3c5cd575c5fa4c1c52449261ac8
Wender <wendermaiac@gmail.com>
2024-08-08 08:15:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine($"Insira a nota do aluno.");
+var input = int.TryParse(Console.ReadLine(), out int nota);
+
+if (nota > 5 && nota <= 10)
+    Console.WriteLine($"o aluno foi aprovado.");
+else if (new[] {4, 5}.Contains(nota)) 
+    Console.WriteLine($"o aluno está de recuperação.");
+else if (nota < 4)
+    Console.WriteLine($"o aluno foi reprovado.");
+else
+    Console.WriteLine($"Nota inválida.");
+
+
